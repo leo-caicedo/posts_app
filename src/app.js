fc0962de74aec4364ec470db1b674c8432fcd00c
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-06 18:32:34 -0500

    ruta dummy de posts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
+// required routes
+const postsRoutes = require("./posts/routes/posts.routes");
+
 const createApp = () => {
   const app = express();
 
   // middleware
   app.use(express.json());
 
+  // routes
+  app.use("/api/posts", postsRoutes);
   return app;
 };
 
