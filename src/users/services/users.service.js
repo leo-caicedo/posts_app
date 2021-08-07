1447cebf81ef2e2b38ef647cb0c6b8538228aa50
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-07 13:12:10 -0500

    servicios para users

---
+++ src/users/services/users.service.js
@@ -0,0 +1,66 @@
+// model
+const User = require("../models/User");
+
+class UsersServices {
+  async getUsers(req, res, next) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async getUser(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      const user = await User.findById(id);
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async createUser(req, res, next) {
+    const { body: data } = req;
+
+    try {
+      const createdUser = new User(data);
+
+      await createdUser.save();
+      res.status(201).json(createdUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async updateUser(req, res, next) {
+    const { body: data } = req;
+    const { id } = req.params;
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(id, data, {
+        new: true,
+      });
+      await updatedUser.save();
+
+      res.json(updatedUser);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  async deleteUser(req, res, next) {
+    const { id } = req.params;
+
+    try {
+      await User.findByIdAndDelete(id);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
+  }
+}
+
+module.exports = UsersServices;
