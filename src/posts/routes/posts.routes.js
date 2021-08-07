98af2fb0d9f616a54bb0432e4fc37b8510d49b26
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-08-06 21:52:33 -0500

    cmabiando los servicios por una clase

--- src/posts/routes/posts.routes.js
+++ src/posts/routes/posts.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
-const router = Router();
 
-router.get("/", (req, res, next) => {
-  res.json({
-    message: "Yours Posts",
-  });
-});
+const router = Router();
+// services
+const PostsServices = require("../services/posts.service");
+const postsServices = new PostsServices();
+
+router.get("/", postsServices.getPosts);
+router.get("/:id", postsServices.getPost);
+router.post("/", postsServices.createPost);
+router.put("/:id", postsServices.updatePost);
+router.delete("/:id", postsServices.deletePost);
 
 module.exports = router;
--- src/posts/services/posts.service.js
+++ src/posts/services/posts.service.js
@@ -1,65 +1,66 @@
-// models
+// model
 const Post = require("../models/Post");
 
-const getPosts = async (req, res, next) => {
-  try {
-    const posts = await Post.find();
-    res.json(posts);
-  } catch (err) {
-    next(err);
+class PostsServices {
+  async getPosts(req, res, next) {
+    try {
+      const posts = await Post.find();
+      res.json(posts);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-const getPost = async (req, res, next) => {
-  const { id } = req.params;
+  async getPost(req, res, next) {
+    const { id } = req.params;
 
-  try {
-    const post = await Post.findById(id);
-    res.json(post);
-  } catch (err) {
-    next(err);
+    try {
+      const post = await Post.findById(id);
+      res.json(post);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-const createPost = async (req, res, next) => {
-  const { body: data } = req;
+  async createPost(req, res, next) {
+    const { body: data } = req;
 
-  try {
-    const postCreated = new Post({ data });
-    await postCreadted.save();
-    res.status(201).json(postCreated);
-  } catch (err) {
-    next(err);
+    try {
+      const createdPost = new Post(data);
+
+      await createdPost.save();
+      res.status(201).json(createdPost);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-const updatePost = async (req, res, next) => {
-  const { id } = req.params;
-  const { body: data } = req;
+  async updatePost(req, res, next) {
+    const { body: data } = req;
+    const { id } = req.params;
 
-  try {
-    const postUpdated = await Post.updateOne({ id }, { data });
-    res.json(postUpdated);
-  } catch (err) {
-    next(err);
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(id, data, {
+        new: true,
+      });
+      await updatedPost.save();
+
+      res.json(updatedPost);
+    } catch (err) {
+      next(err);
+    }
   }
-};
 
-const deletePost = async (req, res, next) => {
-  const { id } = req.params;
+  async deletePost(req, res, next) {
+    const { id } = req.params;
 
-  try {
-    await Post.deleteOne({ id });
-    res.status(204).end();
-  } catch (err) {
-    next(err);
+    try {
+      await Post.findByIdAndDelete(id);
+      res.status(204).end();
+    } catch (err) {
+      next(err);
+    }
   }
-};
+}
 
-module.exports = {
-  getPosts,
-  getPost,
-  createPost,
-  updatePost,
-  deletePost,
-};
+module.exports = PostsServices;
